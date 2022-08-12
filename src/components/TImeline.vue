3ee91b466ed6161a4ba0f62257b754c71f01df35
Lekayie josh - Mint <jlekayie@gmail.com>
2022-08-12 03:52:11 +0300

    Change to Vue app

---
+++ src/components/TImeline.vue
@@ -0,0 +1,38 @@
+<template>
+  <vertical-timeline :inputData="mockedData" :reversed="false" />
+</template>
+
+<script>
+import mock from "./mock.json";
+
+export default {
+  name: "Helloworld",
+  data() {
+    return {
+      mockedData: mock
+    };
+  }
+};
+</script>
+
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type;
+  none,
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color;
+  #42b983;
+}
+</style>
